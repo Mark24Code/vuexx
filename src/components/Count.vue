eae5dfbf4a3130e2a91b5dfdc86076fc1204c2b7
mark24 <mark.zhangyoung@qq.com>
2016-12-26 23:42:49 +0800

    demo

---
+++ src/components/Count.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <div><p>Hello Components</p></div>
+    <div>
+      {{count}}
+    </div>
+    <div>
+      <button @click="increment">Inc</button>
+      <button @click="decrement">Dec</button>
+    </div>
+  </div>
+</template>
+<script>
+import {mapGetters} from 'vuex'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed:mapGetters([
+      'count'
+  ]),
+  methods:{
+    increment(){
+      this.$store.dispatch("inc")
+    },
+    decrement(){
+      this.$store.dispatch("dec")
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/vuex/store.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+    count:500
+}
+
+const mutations = {
+    INC(state){
+        state.count++
+    },
+    DEC(state){
+        state.count--
+    }
+}
+
+const actions = {
+    inc({commit}){
+        commit('INC')
+    },
+    dec({commit}){
+        commit('DEC')
+    }
+}
+
+const getters = {
+    count:(state)=>{
+        return state.count
+    }
+}
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    actions,
+    getters
+})
