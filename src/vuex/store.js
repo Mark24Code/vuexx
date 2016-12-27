95ea4f99a48d7e7c199a60743f1a7307ac8a9499
Mark24 <yang.zhang@3green-tech.com>
2016-12-27 11:48:03 +0800

    store拆分：分化成功

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,34 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
 
 Vue.use(Vuex)
 
 const state = {
     count:500
-}
-
-const mutations = {
-    INC(state){
-        state.count++
-    },
-    DEC(state){
-        state.count--
-    }
-}
-
-const actions = {
-    inc({commit}){
-        commit('INC')
-    },
-    dec({commit}){
-        commit('DEC')
-    }
-}
-
-const getters = {
-    count:(state)=>{
-        return state.count
-    }
 }
 
 export default new Vuex.Store({
